8618af5c4313d292d306c24c00d63ef026f8be53
Anas A. Ibrahim <74794601+AnasIbrahim1@users.noreply.github.com>
2022-08-30 16:14:21 +0200

    Create template.cpp

---
+++ All_templates/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int tt, tc;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    tt = 1, tc = 1; cin >> tt;
+    while (tt--) solve(), tc++;
+}
