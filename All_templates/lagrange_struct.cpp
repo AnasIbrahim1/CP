7fee387530c7917bcbe0c82b4c4258882d10585f
Anas A. Ibrahim <74794601+AnasIbrahim1@users.noreply.github.com>
2024-04-20 03:01:04 +0200

    Update lagrange_struct.cpp

--- All_templates/lagrange_struct.cpp
+++ All_templates/lagrange_struct.cpp
@@ -1,6 +1,7 @@
-
 template <class T>
-struct lagrange { 
+struct lagrange{
+    vector<pair<T, T>> p; // assumes the first's are distinct
+    lagrange(vector<pair<T, T>> points) : p(points) {} 
     T evaln(T x) { // assumes the step between points are constant and the points are increasing (in x-coordinates)
         for (auto& [v, y] : p) if (v == x) return y;
         T ans{};
@@ -10,10 +11,7 @@ struct lagrange {
             prod_denom = prod_denom * (p[0].first - p[i].first);
         }
         ans = ans + (prod_num / prod_denom) * p[0].second;
- 
-        T step = p[1].first - p[0].first;
-        T r = p[0].first - p.back().first;
-        T l = step;
+        T step = p[1].first - p[0].first, r = p[0].first - p.back().first, l = step; 
         for (int i = 1; i < (int)p.size(); i++) {
             T v = p[i].first, y = p[i].second;
             prod_num = prod_num * (x - p[i - 1].first);
@@ -21,8 +19,10 @@ struct lagrange {
  
             prod_denom = prod_denom * l;
             prod_denom = prod_denom / r;
+ 
             l = l + step;
             r = r + step;
+ 
             ans = ans + (prod_num / prod_denom) * y;
         }
         return ans;
