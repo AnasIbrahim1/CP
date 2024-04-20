8f8ed197d707bbd00d1084ccd22474f661d0cafd
Anas A. Ibrahim <74794601+AnasIbrahim1@users.noreply.github.com>
2024-04-20 02:56:09 +0200

    Create lagrange_struct.cpp

---
+++ All_templates/lagrange_struct.cpp
@@ -0,0 +1,30 @@
+
+template <class T>
+struct lagrange { 
+    T evaln(T x) { // assumes the step between points are constant and the points are increasing (in x-coordinates)
+        for (auto& [v, y] : p) if (v == x) return y;
+        T ans{};
+        T prod_num = T(1), prod_denom = T(1);
+        for (int i = 1; i < (int)p.size(); i++) {
+            prod_num = prod_num * (x - p[i].first);
+            prod_denom = prod_denom * (p[0].first - p[i].first);
+        }
+        ans = ans + (prod_num / prod_denom) * p[0].second;
+ 
+        T step = p[1].first - p[0].first;
+        T r = p[0].first - p.back().first;
+        T l = step;
+        for (int i = 1; i < (int)p.size(); i++) {
+            T v = p[i].first, y = p[i].second;
+            prod_num = prod_num * (x - p[i - 1].first);
+            prod_num = prod_num / (x - p[i].first);
+ 
+            prod_denom = prod_denom * l;
+            prod_denom = prod_denom / r;
+            l = l + step;
+            r = r + step;
+            ans = ans + (prod_num / prod_denom) * y;
+        }
+        return ans;
+    }
+};
